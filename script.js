7014c5fd8a94970ad5afc1dcaff98856fb3a4757
khaled-bgrr <bougherrakhaled@gmail.com>
2024-05-02 15:25:03 +0100

    Added a UI cleanup

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 //Add items to the list via form
-const ul = document.querySelector("#item-list");
+const itemList = document.querySelector("#item-list");
 const input = document.querySelector(".form-input");
 const formBtn = document.querySelector(".btn");
-
+const clearBtn = document.querySelector("#clear");
+const filter = document.querySelector(".filter");
 const addItem = (e) => {
   e.preventDefault();
   //Creating the elements for a new task
@@ -13,6 +14,7 @@ const addItem = (e) => {
   } else {
     console.log("success");
     const li = document.createElement("li");
+    li.classList = "remove";
     li.innerText = input.value;
     console.log(li);
     const btn = document.createElement("button");
@@ -22,8 +24,9 @@ const addItem = (e) => {
     icon.classList.add("fa-solid", "fa-xmark");
     btn.appendChild(icon);
     li.appendChild(btn);
-    ul.appendChild(li);
+    itemList.appendChild(li);
     input.value = "";
+    checkUI();
   }
 };
 
@@ -32,10 +35,34 @@ formBtn.addEventListener("click", addItem);
 //Removing items by clicking X
 
 const removeItem = (e) => {
-  e.target.parentElement.parentElement.remove();
+  if (e.target.parentElement.classList.contains("remove-item")) {
+    if (confirm("Are you sure ?")) {
+      e.target.parentElement.parentElement.remove();
+    }
+    checkUI();
+  } else {
+    console.log("not the X mark");
+  }
 };
 
-const deleteBtns = document.querySelectorAll(".remove-item");
-deleteBtns.forEach((Xbtn) => {
-  Xbtn.addEventListener("click", removeItem);
-});
+const clearAll = () => {
+  while (itemList.firstChild) {
+    itemList.firstChild.remove();
+  }
+  checkUI();
+};
+//Clean UI, removing the filter field and the clear btn when there is no items
+const checkUI = () => {
+  const items = itemList.querySelectorAll("li");
+  if (items.length === 0) {
+    filter.style.display = "none";
+    clearBtn.style.display = "none";
+  } else {
+    filter.style.display = "block";
+    clearBtn.style.display = "block";
+  }
+};
+console.log(itemList);
+itemList.addEventListener("click", removeItem);
+clearBtn.addEventListener("click", clearAll);
+checkUI();
